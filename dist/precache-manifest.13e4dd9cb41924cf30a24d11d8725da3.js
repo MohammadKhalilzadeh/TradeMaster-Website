577d36af0863a0dfcc98b5fbd9a05f34e951328c
Mohammad Khalilzadeh <46302149+MohammadKhalilzadeh@users.noreply.github.com>
2022-02-11 11:16:10 +0330

    Add files via upload

---
+++ dist/precache-manifest.13e4dd9cb41924cf30a24d11d8725da3.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b121b42f12cbc8a78159",
+    "url": "/css/app.5b25ced4.css"
+  },
+  {
+    "revision": "c5f4d4ffdb7c948a0e99d7fa48c699e8",
+    "url": "/img/EC.c5f4d4ff.png"
+  },
+  {
+    "revision": "448a39b70087fe56bc5f72e86bb72f53",
+    "url": "/img/arm.448a39b7.jpg"
+  },
+  {
+    "revision": "29c49156b4a172f10bc2cc57b5886c0d",
+    "url": "/img/bagh.29c49156.jpg"
+  },
+  {
+    "revision": "e0f4b3ed6e95d4e531d15b3b31489b25",
+    "url": "/img/book1.e0f4b3ed.jpg"
+  },
+  {
+    "revision": "e7d4f30d027cf7ee9a42a6fe7ad2801f",
+    "url": "/img/fk.e7d4f30d.png"
+  },
+  {
+    "revision": "625fc49fbe2201ff2e3454cfddb9167f",
+    "url": "/img/redsign.625fc49f.jpg"
+  },
+  {
+    "revision": "937f91b13db96728f7ceaf0c7cc16bcb",
+    "url": "/img/redsign2.937f91b1.jpg"
+  },
+  {
+    "revision": "ccacf6e7afa547d552db16611a58e8b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b121b42f12cbc8a78159",
+    "url": "/js/app.234853e8.js"
+  },
+  {
+    "revision": "c33c8b721a755f725e7f",
+    "url": "/js/chunk-vendors.c89c78e5.js"
+  },
+  {
+    "revision": "a59a9466225db10c227b4a2f543e95d2",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
